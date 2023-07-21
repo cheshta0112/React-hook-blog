534517455576f1dd49baec2a93e23902e80dd16e
Cheshta Pal <104692214+cheshta0112@users.noreply.github.com>
2023-07-21 15:30:07 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './components/App';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
